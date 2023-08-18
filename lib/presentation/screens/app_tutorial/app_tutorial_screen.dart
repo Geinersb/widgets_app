d7020e8ef4b1f5133408facfeb0e001c0628eacc
Geinersb <geinersb20@gmail.com>
2023-08-17 20:29:24 -0600

    se finaliza el intro de una app page view

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -22,10 +23,39 @@ final slides = <SlideInfo>[
       'assets/images/3.png'),
 ];
 
-class AppTutorialScreen extends StatelessWidget {
+class AppTutorialScreen extends StatefulWidget {
   static const name = 'tutorial_screen';
 
   const AppTutorialScreen({super.key});
+
+  @override
+  State<AppTutorialScreen> createState() => _AppTutorialScreenState();
+}
+
+class _AppTutorialScreenState extends State<AppTutorialScreen> {
+  final PageController pageViewController = PageController();
+  bool endReached = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    pageViewController.addListener(() {
+      final page = pageViewController.page ?? 0;
+
+      if (!endReached && page >= (slides.length - 1.5)) {
+        setState(() {
+          endReached = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    pageViewController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,21 +64,32 @@ class AppTutorialScreen extends StatelessWidget {
       body: Stack(
         children: [
           PageView(
-              physics: const BouncingScrollPhysics(),
-              children: slides
-                  .map((slideData) => _Slide(
-                      title: slideData.title,
-                      caption: slideData.caption,
-                      imageUrl: slideData.imageUrl))
-                  .toList(),             
-                                   
-                  ),
-
-                  Positioned(
-                    right: 20,
-                    top: 50,
-                    child: TextButton(onPressed: () =>context.pop() , 
-                    child: const Text('Salir')))
+            controller: pageViewController,
+            physics: const BouncingScrollPhysics(),
+            children: slides
+                .map((slideData) => _Slide(
+                    title: slideData.title,
+                    caption: slideData.caption,
+                    imageUrl: slideData.imageUrl))
+                .toList(),
+          ),
+          Positioned(
+              right: 20,
+              top: 50,
+              child: TextButton(
+                  onPressed: () => context.pop(), child: const Text('Salir'))),
+          endReached
+              ? Positioned(
+                  bottom: 30,
+                  right: 30,
+                  child: FadeInRight(
+                    from: 15,
+                    delay: const Duration(seconds: 1),
+                    child: FilledButton(
+                        onPressed: () => context.pop(),
+                        child: const Text('Comenzar')),
+                  ))
+              : const SizedBox(),
         ],
       ),
     );
@@ -79,7 +120,7 @@ class _Slide extends StatelessWidget {
             const SizedBox(height: 20),
             Text(title, style: titleStyle),
             const SizedBox(height: 10),
-            Text(caption,style: captionStyle),
+            Text(caption, style: captionStyle),
           ],
         ),
       ),
